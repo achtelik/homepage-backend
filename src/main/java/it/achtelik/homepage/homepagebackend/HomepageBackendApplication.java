d8f8553692c7f9975fc58779fa27489280bef280
Matthias <ma@ma-tech.de>
2021-08-16 19:41:16 +0000

    Init

---
+++ src/main/java/it/achtelik/homepage/homepagebackend/HomepageBackendApplication.java
@@ -0,0 +1,13 @@
+package it.achtelik.homepage.homepagebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomepageBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomepageBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/achtelik/homepage/homepagebackend/HomepageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.achtelik.homepage.homepagebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomepageBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
